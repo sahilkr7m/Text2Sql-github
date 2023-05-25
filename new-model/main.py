ba85b669920778d6e62d3b346c6de4461df837c0
Aman Bhatt <bhatt.codes@gmail.com>
2023-05-25 16:28:48 +0530

    added query generator

--- new-model/main.py
+++ new-model/main.py
@@ -7,7 +7,7 @@ from collections import deque
 nlp = spacy.load("en_core_web_sm")
 
 # sentence = "get all emails phone names age gender from database db2 where age is greater than 25 and phoneno is equal 10 order by name in ascending and gender equal to male and phone equal 20"
-sentence = "where age is more than 40 count name"
+sentence = "get name email phoneno address where age greater than 30 and gender equal to male "
 doc = nlp(sentence)
 
 columns_present_in_DB = [
@@ -25,23 +25,39 @@ operator_mapping = {
     "less" : "<",
     "equal" : "=",
     "more" : ">",
-    "ascending" : "ASC",
-    "descending" : "DESC",
+    # "ascending" : "ASC",
+    # "descending" : "DESC",
 }
 
-reserved_keywords = ["get","give","find","from","where","and", "order","group","having","count"]
+reserved_keywords = ["get","give","find","from","where","and", "order","group","having","count","ascending","descending"]
+
+def map_column_with_db(text):
+     for key in columns_present_in_DB:
+                similarity_ratio = difflib.SequenceMatcher(None, key, text).ratio()
+                if(similarity_ratio>0.8):
+                    return key
 
 
+def map_input_column_array_with_db_columns(input_columns):
+    mapped_array=[]
+       
+    for text in input_columns:
+       if(map_column_with_db(text)):
+            mapped_array.append(map_column_with_db(text))
+
+    return mapped_array
+    
+# def operator_mapping_function(text):
+#     for key,value in operator_mapping.items():
+#             similarity_ratio = difflib.SequenceMatcher(None, key, text).ratio()
+#             if(similarity_ratio>0.8):
+#                 return value
 
 def filter_token(text, pos ):
     best_match = ""
     if(text in reserved_keywords):
         best_match = text
     elif(pos =="NOUN"):
-        # for key in columns_present_in_DB:
-        #     similarity_ratio = difflib.SequenceMatcher(None, key, text).ratio()
-        #     if(similarity_ratio>0.8):
-        #         best_match = key
         best_match = text
 
     elif(pos== "NUM"):
@@ -92,33 +108,112 @@ def extracting_info(filtered_tokens):
                 from_database.append(filtered_tokens[it])
             i=end_index
         elif(filtered_tokens[i]=="where"):
-
-            for it in range(i+1, i+4):
-                where_clause.append(filtered_tokens[it])
+            obj = {
+                "col": map_column_with_db(filtered_tokens[i+1]),
+                "op": filtered_tokens[i+2],
+                "val": filtered_tokens[i+3],
+            }
+            where_clause.append(obj )
+            
+            # for it in range(i+1, i+4):                
+            #     where_clause.append(filtered_tokens[it])
             i=i+4
         elif(filtered_tokens[i]=="and"):
+
             if(filtered_tokens[i+1] in columns_present_in_DB):
-                for it in range(i+1, i+4):
-                    where_clause.append(filtered_tokens[it])
+                obj = {
+                "col": map_column_with_db(filtered_tokens[i+1]),
+                "op": filtered_tokens[i+2],
+                "val": filtered_tokens[i+3],
+                }
+                where_clause.append(obj )
+            
             i=i+4
+
         elif(filtered_tokens[i]=="order"):
             if(filtered_tokens[i+1] in columns_present_in_DB):
-                for it in range(i+1, i+3):
-                    orderby_clause.append(filtered_tokens[it])
+
+                orderby_clause.append(map_column_with_db(filtered_tokens[i+1]))
+                orderby = ""
+                if(difflib.SequenceMatcher(None, filtered_tokens[i+2], "ascending").ratio()>0.8):
+                    orderby="ASC"
+                elif(difflib.SequenceMatcher(None, filtered_tokens[i+2], "descending").ratio()>0.8):
+                    orderby="DESC"
+                orderby_clause.append(orderby)
+                # for it in range(i+1, i+3):
+                #     orderby_clause.append(filtered_tokens[it])
             i=i+3
-
-
-
-
         else:
             i+=1
 
-    
-    return [selected_columns,
+
+
+    return [ map_input_column_array_with_db_columns(selected_columns),
     from_database ,
     where_clause, orderby_clause, count]
 
         
+
+def generate_query(query_array):
+    selected_columns =query_array[0]
+    from_database = query_array[1]
+    where_clause = query_array[2]
+    orderby_clause =query_array[3]
+    count =query_array[4]
+
+    query = ""
+    comma_count=0
+    comma_count_max = len(selected_columns)-1
+    if(count==1):
+        query+="SELECT COUNT("
+        for i in selected_columns:
+            query+= i
+            if(comma_count<comma_count_max):
+                query+=","
+                comma_count+=1
+        
+        query+=")"
+        
+        query+= " "
+    else:
+
+        query+="SELECT "
+        for i in selected_columns:
+            query+=i
+            if(comma_count<comma_count_max):
+                query+=","
+                comma_count+=1
+        
+        query+= " "
+
+    if(len(from_database)==0):
+        query+= "FROM DB"
+        
+        query+= " "
+
+    if(len(where_clause)!=0):    
+        and_count_max = len(where_clause) -1
+        and_count=0
+
+        query+= "WHERE "
+        for item in where_clause:
+            query+= item["col"] + " " + item["op"] + " " + item["val"]+ " "
+            if(and_count<and_count_max):
+                query+="AND"
+                and_count+=1
+            
+            query+= " "
+
+    if(len(orderby_clause)!=0):
+        query+="ORDER BY " + orderby_clause[0]+ " "+ orderby_clause[1]
+        
+
+
+
+
+
+    return query
+    
 
 
 
@@ -148,5 +243,6 @@ for token in doc:
 
 print(filtered_tokens)
 print(str(extracting_info(filtered_tokens)))
+print(generate_query(extracting_info(filtered_tokens)))
 # print("checking")
 # print(difflib.SequenceMatcher(None, "gerater", "greater").ratio())
